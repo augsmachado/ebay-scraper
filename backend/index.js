a57b710dab26ec6ff67721432dcfee862081baee
Augs Machado <augustomachadoramos@gmail.com>
2022-10-22 23:01:51 -0300

    feat: add caching

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import NodeCache from "node-cache";
+
 import status from "./routes/status.routes.js";
 import products from "./routes/products.routes.js";
 import deals from "./routes/deals.routes.js";
@@ -10,12 +12,27 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Define node caching settings > default 15 seconds
+const cache = new NodeCache({ stdTTL: 15 });
+const verifyCache = (req, res, next) => {
+	try {
+		const { id } = req.params;
+		if (cache.has(id)) {
+			console.log(`cache`);
+			return res.status(200).json(cache.get(id));
+		}
+		return next();
+	} catch (err) {
+		throw new Error(err);
+	}
+};
+
 app.use(express.json());
 
-app.use("/", status);
-app.use("/status", status);
-app.use("/products", products);
-app.use("/deals", deals);
+app.use("/", verifyCache, status);
+app.use("/status", verifyCache, status);
+app.use("/products", verifyCache, products);
+app.use("/deals", verifyCache, deals);
 
 app.use("*", (req, res) => {
 	res.status(400).json({ error: "Not route found" });
